b166ae601a74b71f779374c2f6d2d0086a3ff1fb
Puwanartlnwza <puwanartlnwza@gmail.com>
2025-05-30 01:32:58 +0700

    Update Graphics.cpp

--- src/mods/Graphics.cpp
+++ src/mods/Graphics.cpp
@@ -384,7 +384,7 @@ void Graphics::do_ultrawide_fix() {
     // This disables any kind of pillarboxing and letterboxing.
     // This cannot be directly restored once applied.
     if (set_display_type_method != nullptr) {
-        set_display_type_method->call(sdk::get_thread_context(), main_view, via::DisplayType::Fit);
+        set_display_type_method->call(sdk::get_thread_context(), main_view, via::DisplayType::Uniform4x3);
     }
 }
 
